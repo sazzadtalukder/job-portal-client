1876e9a405aa9ba9c2a0ab2ca052a4ed3ddfaba6
Md. Sazzad Talukder <sazzadtalukdercseiu@gmail.com>
2025-07-21 00:43:26 +0600

    Lorem added in home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import HotJobs from './HotJobs';
 const Home = () => {
     return (
         <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, totam doloribus, illum eius quam, esse nobis voluptate expedita debitis sint facere sunt hic. Voluptatem eligendi numquam dolorum, iste aliquid obcaecati?</p>
             <Banner></Banner>
             <HotJobs></HotJobs>
         </div>
