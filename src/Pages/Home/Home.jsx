acff4102643145bc11831e8e2c2588d010663be1
Md. Sazzad Talukder <sazzadtalukdercseiu@gmail.com>
2025-07-21 00:46:13 +0600

    change in main branch

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,6 +7,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <HotJobs></HotJobs>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi dolores error explicabo alias quas vitae amet illum quos ab officia soluta cum, corporis, et aperiam recusandae minima provident, ipsa commodi.</p>
         </div>
     );
 };
